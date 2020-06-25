4a3a9d3a4309e97c363b8025e455a7293ea0949a
Daklin2 <daklin754@gmail.com>
2020-06-25 03:25:05 +0300

    feat: style auth page

--- test-task/src/index.jsx
+++ test-task/src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 
 import App from './components/App/';
 import store from './redux/redux-store';
+import './styles-global/index.module.scss';
 
 ReactDOM.render(
   <Provider store={store}>
--- test-task/src/redux/reducers/index.js
+++ test-task/src/redux/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 
 import authReducer from './auth.reducer';
 
-const reducer = combineReducers(authReducer);
+const reducer = combineReducers({
+  auth: authReducer
+});
 
 export default reducer;
