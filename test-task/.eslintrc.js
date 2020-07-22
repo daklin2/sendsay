845fc80c373d408439326c5421cdf9652eac1073
Daklin2 <daklin754@gmail.com>
2020-07-22 10:26:14 +0300

    docs: add rules to lint

--- test-task/.eslintrc.js
+++ test-task/.eslintrc.js
@@ -18,5 +18,10 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'react/jsx-curly-newline': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/no-noninteractive-tabindex': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
   },
 };
